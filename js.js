8706c7acc03b04894474ee0ab5c485768624b0f7
wsteem <102566225+wsteem@users.noreply.github.com>
2024-04-16 08:40:30 -0600

    changes

--- js.js
+++ js.js
@@ -26,3 +26,57 @@ map.addEventListener("mousemove", function(e){
     let mouseY = String(map.offsetHeight - e.offsetY);
     mouse_pos.innerHTML = mouseX.concat(", ").concat(mouseY);
 })
+
+
+
+
+
+//CODE FOR SEARCH BAR//
+
+let availableKeywords = [
+    'Home' ,
+    'About' ,
+    'Team' ,
+];
+
+//Next few lines are used to display the available words in the search drum//
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+//When user starts typing anything the search box, this function will be executed//
+inputBox.onkeyup = function(){
+    //stores all the filtered key words//
+    let result = [];
+    let input = inputBox.value;
+    //This will check what keywords are similar to the available words//
+    if(input.length){
+        result = availableKeywords.filter((keyword)=>{
+            //checks any keyword and relates it to available words, whether it be LowerCase or UpperCase//
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result);
+
+    //To hide the small portion of the div that shows even when no available keywords relate to the searched item//
+    if(!result.length){
+        resultsBox.innerHTML = '';
+    }
+}
+
+//funtion that will display result from available keywords based on keyword searched by the user//
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });
+    
+    //to remove commas after available keywords, the .join('') adds empty space between its '' to replace the comma//
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+
+//To have a realted keyword autofill the search bar after the user clicks on it//
+function selectInput(list){
+    inputBox.value = list.innerHTML
+    //To hide all available keywords that originally appeared after the user clicks on one of them//
+    resultsBox.innerHTML = '';
+}
